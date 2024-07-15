09ca6463d2cad4d1da7943fc99fccd0da1d5bc88
Peter30arproyectos <peter30ar.proyectos@gmail.com>
2024-07-15 19:58:42 -0300

    agrego Clase 32

---
+++ CLASES/Clase32/API_CRUD/models/posteosModel.js
@@ -0,0 +1,20 @@
+ // importo la base de datos
+
+const db = require ("../data/db.js")
+
+const {DataTypes} = require ("sequelize")
+
+
+
+const posteosModel =db.define("posteos",{
+titulo:{type:DataTypes.STRING},
+contenido:{type:DataTypes.STRING},
+})
+
+module.exports = posteosModel
+
+
+
+
+
+
